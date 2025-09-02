a52476982c384e188ea43dee3629e8989304390e
aidencullo <aidencullo@gmail.com>
2025-09-02 15:40:33 -0400

    Add SimpleComponent and integrate it in main page

---
+++ app/components/SimpleComponent.tsx
@@ -0,0 +1,8 @@
+export default function SimpleComponent() {
+  return (
+    <div className="p-4 bg-blue-100 border border-blue-300 rounded-lg">
+      <h2 className="text-xl font-bold text-blue-800">Simple Component</h2>
+      <p className="text-blue-600">This is a simple component!</p>
+    </div>
+  );
+}
